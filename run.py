ba839bab54d10e47b427f4cea2c53d484616a5cc
Boubou78000 <142528590+Boubou78000@users.noreply.github.com>
2024-02-22 13:50:53 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import os
 Secret=os.getenv("Secret")
 Session=s3.login("Boubou78000", Secret)
 Conn=Session.connect_cloud("26861174") #Sorry Griff
-Conn.set_var("Cloud 1", "0xffffff")
+Conn.set_var("Cloud 1", "0")
